956bc3d0766ab32886e7cf6134c6b85c18122499
Philip Huysmans <philip.huysmans@gmail.com>
2015-12-30 21:26:50 +0100

    strange things with rkclient

--- rkClient/index.js
+++ rkClient/index.js
@@ -11,6 +11,14 @@ var runkeeper = require('runkeeper-js');
 
 var client = exports.client = new runkeeper.HealthGraph(options);
 
+var redis = require('redis');
+var redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_URL, {no_ready_check: true});
+redisClient.auth(process.env.REDIS_PASSWD, function (err) {
+    if (err) {
+      console.log(err);
+    }
+});
+
 exports.getTokenFromRunkeeper = function(authorizationCode) {
 	return new Promise(function(resolve, reject) {
 		console.log("Getting access token from RunKeeper.");
@@ -31,3 +39,32 @@ exports.setToken = function(accessToken) {
 	return accessToken;
 }
 
+function persistToken(token) {
+	console.log("Staring to persist");
+	return new Promise(function(resolve,reject) {
+		redisClient.set("access_token", token, function(err,reply) {
+			if(err) {
+				reject(err);
+			} else {
+				resolve(reply);
+			}
+		});
+	});
+}
+
+exports.loadToken = function() {
+	return new Promise(function(resolve,reject) {
+		redisClient.get("access_token", function(err,reply) {
+			if(err) {
+				reject(err);
+			} else {
+				if(reply) {
+					client.access_token = reply;
+					resolve(reply);	
+				} else {
+					reject("No access token");
+				}
+			}
+		});
+	});
+}
