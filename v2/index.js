e6b58a4048fac7b526116042ce7134f1a329009e
Philip Huysmans <philip.huysmans@gmail.com>
2016-12-06 09:37:49 +0100

    addition

--- v2/index.js
+++ v2/index.js
@@ -1,9 +1,13 @@
 require('dotenv').load({path: '.env'});
 
 var redis = require('redis');
+var runkeeper = require('runkeeper-js');
+
 var RunkeeperRedis = require('./runkeeper/runkeeper.redis.server');
+var RunkeeperClient = require('./runkeeper/runkeeper.client.server');
 
 var rkRedis = new RunkeeperRedis(redis);
+var rkClient = new RunkeeperClient(runkeeper, rkRedis);
 
 rkRedis.persistToken("Blablalba")
 	.then(function(res) {
---
+++ v2/runkeeper/runkeeper.client.server.js
@@ -0,0 +1,92 @@
+var RunkeeperClient = function(runkeeper, rkredis) {
+	var options = {
+		client_id: process.env.CLIENTID,
+		client_secret: process.env.CLIENTSECRET,
+		auth_url: "https://runkeeper.com/apps/authorize",
+		access_token_url: "https://runkeeper.com/apps/token",
+		redirect_uri: process.env.REDIRECTURI,
+		api_domain: "api.runkeeper.com"
+	};
+
+	var client = new runkeeper.HealthGraph(options);
+
+	this.getTokenFromRunkeeper = function(authorizationCode) {
+		return new Promise(function(resolve, reject) {
+			console.log("RKCLIENT: Getting access token from RunKeeper.");
+			client.getNewToken(authorizationCode, function(err, accessToken) {
+				if(err) {
+					console.log("RKCLIENT: Error getting access token: " + err);
+					reject(err);
+				} else {
+					resolve(accessToken);
+				}
+			});
+		});
+	};
+};
+
+module.exports = RunkeeperClient;
+
+/*
+
+
+
+var redis = require('redis');
+var redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_URL, {no_ready_check: true});
+redisClient.auth(process.env.REDIS_PASSWD, function (err) {
+    if (err) {
+      console.log(err);
+    }
+});
+
+exports.getTokenFromRunkeeper = function(authorizationCode) {
+	return new Promise(function(resolve, reject) {
+		console.log("Getting access token from RunKeeper.");
+		client.getNewToken(authorizationCode, function(err, accessToken) {
+	    if(err) {
+	      console.log("Error getting access token: " + err);
+	      reject(err);
+	    } else {
+	    	resolve(accessToken);
+	    }
+		});
+	});
+}
+
+exports.setToken = function(accessToken) {
+	console.log("Setting access token in local client.");
+	client.access_token = accessToken;
+	return accessToken;
+}
+
+function persistToken(token) {
+	console.log("Staring to persist");
+	return new Promise(function(resolve,reject) {
+		redisClient.set("access_token", token, function(err,reply) {
+			if(err) {
+				reject(err);
+			} else {
+				resolve(reply);
+			}
+		});
+	});
+}
+
+exports.loadToken = function() {
+	return new Promise(function(resolve,reject) {
+		redisClient.get("access_token", function(err,reply) {
+			if(err) {
+				reject(err);
+			} else {
+				if(reply) {
+					client.access_token = reply;
+					resolve(reply);	
+				} else {
+					reject("No access token");
+				}
+			}
+		});
+	});
+}
+
+*/
